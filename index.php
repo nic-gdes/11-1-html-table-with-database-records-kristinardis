2976b4a00940d4ce248d8c6389b2d2d3e094e4bc
kristinardis <kamcllvenna@nic.edu>
2024-03-24 21:15:40 -0700

    Website works and shows data in rows and columns
    Had to run the mysql  manually because phpmyadmin was not working and responded with "500 Bad Gateway" error.

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 // include the database connection
 include './config/db.php';
 
+/** Had to run this manually because phpmyadmin was not working and responded with "500 Bad Gateway" error */
+/**
+$createSql = "CREATE TABLE items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    price FLOAT
+)";
+$create = mysqli_query($conn, $createSql);
+
+$insetSql = "INSERT INTO items (name, price) VALUES ('Laptop', 1000), ('Phone', 500), ('Tablet', 300)";
+$insert = mysqli_query($conn, $insetSql);
+*/
+
 // create a query
 $sql = "SELECT name, price FROM items";
 
