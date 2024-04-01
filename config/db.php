26d4f145fa18592d6005fdf371142c2c5487cd04
kristinardis <kamcllvenna@nic.edu>
2024-04-01 07:22:03 -0700

    attempt to uninstall ddev and reinstall

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 
-$conn = new mysqli("db", "root", "root", "db");
-
+//$conn = new mysqli("db", "root", "root", "db");
+$conn = mysqli_connect("mysql.server297.com:3308","2024kmcilvenna","kmcilvenna939", "welearnd_gdes261_2024_kmcilvenna");
 
 if (mysqli_connect_errno()) {
     echo "Database error: " . mysqli_connect_error();
